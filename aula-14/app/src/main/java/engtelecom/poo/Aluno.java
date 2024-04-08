36925e66dbeec39f7a646aab964c876f0ccb88d2
Luan de Barros <luanbarrossantos@gmail.com>
2024-04-08 11:30:02 -0300

    Inicio de hash map

---
+++ aula-14/app/src/main/java/engtelecom/poo/Aluno.java
@@ -0,0 +1,49 @@
+package engtelecom.poo;
+
+import java.util.Date;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private String cpf;
+    private Date dataDeNascimento;
+
+    public Aluno(String nome, String matricula, String cpf, Date dataDeNascimento) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cpf = cpf;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
