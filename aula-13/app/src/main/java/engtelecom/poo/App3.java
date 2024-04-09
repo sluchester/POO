584236c2b17bbd75eed7eced44e64b7050eb9f40
Luan de Barros <luanbarrossantos@gmail.com>
2024-04-08 21:51:31 -0300

    exercício app3 funcionando

--- aula-13/app/src/main/java/engtelecom/poo/App3.java
+++ aula-13/app/src/main/java/engtelecom/poo/App3.java
@@ -6,8 +6,33 @@
 
 package engtelecom.poo;
 
+import java.util.*;
+
 public class App3 {
     public static void main(String[] args) {
+        //Queue<Integer> queue = new LinkedList<>();
+        ArrayDeque<Integer> deque= new ArrayDeque<>();
 
+        Scanner scanner = new Scanner(System.in);
+
+        int readNumber = 0;
+
+        while(readNumber != 999){
+            System.out.println("digite um numero ");
+            readNumber = scanner.nextInt();
+
+            if(readNumber == 999){
+                break;
+            }else{
+                deque.add(readNumber);
+            }
+        }
+
+        while (!deque.isEmpty()){
+            System.out.println(deque.removeLast());
+        }
+        /*for (int i = queue.size(); i > 0; i--) {
+            queue.re
+        }*/
     }
 }
