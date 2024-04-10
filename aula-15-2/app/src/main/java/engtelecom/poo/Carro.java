54102d9432b942575b13cf50be38b6c30ef33b8f
Luan de Barros <luanbarrossantos@gmail.com>
2024-04-10 11:28:25 -0300

    iniciando a dinamica de agregacao e composicao entre classes

--- aula-15-2/app/src/main/java/engtelecom/poo/Carro.java
+++ aula-15-2/app/src/main/java/engtelecom/poo/Carro.java
@@ -1,4 +1,24 @@
 package engtelecom.poo;
 
 public class Carro {
+    private String modelo;
+    private String marca;
+    private Motor motor;
+    //carro contem motor
+
+
+    public Carro(String modelo, String marca, Motor motor) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.motor = motor;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", motor=" + motor +
+                '}';
+    }
 }
--- aula-15-2/app/src/main/java/engtelecom/poo/Motor.java
+++ aula-15-2/app/src/main/java/engtelecom/poo/Motor.java
@@ -1,4 +1,22 @@
 package engtelecom.poo;
 
 public class Motor {
+    private int hp;
+    private int cilindros;
+    private double vMax;
+
+    public Motor(int hp, int cilindros, double vMax) {
+        this.hp = hp;
+        this.cilindros = cilindros;
+        this.vMax = vMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Motor{" +
+                "hp=" + hp +
+                ", cilindros=" + cilindros +
+                ", vMax=" + vMax +
+                '}';
+    }
 }
