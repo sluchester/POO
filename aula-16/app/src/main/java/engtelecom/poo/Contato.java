b22ca7b984a17692d704c334a1248f4a0b0dd7b0
Luan de Barros <luanbarrossantos@gmail.com>
2024-04-29 20:44:24 -0300

    finalizada a classe

--- aula-16/app/src/main/java/engtelecom/poo/Contato.java
+++ aula-16/app/src/main/java/engtelecom/poo/Contato.java
@@ -8,9 +8,7 @@ public class Contato {
     private String nome;
     private String sobreNome;
     private LocalDate dataDeNascimento;
-    //tentar usar a classe telefone para retornar rotulo e valor
     private ArrayList<Telefone> dadosDeTelefone;
-    //tentar usar a classe email para retornar rotulo e valor
     private ArrayList<Email> dadosDeEmail;
 
     public Contato(String nome, String sobreNome, LocalDate dataDeNascimento) {
@@ -33,47 +31,77 @@ public class Contato {
     public boolean addTelefone(String rotulo, String numero){
         dadosDeTelefone.add(new Telefone(rotulo, numero));
         return true;
+        //validar o return dele
     }
 
+    //valida email é aqui ou dentro de email?
     public boolean addEmail(String rotulo, String email){
-        /*if(validaEmail(email)){
-            dadosDeEmail.add(new Email(rotulo, email));
-            return true;
-        } else{
-            return false;
-        }*/
+        dadosDeEmail.add(new Email(rotulo, email));
         return true;
+        //validar o return dele
     }
 
-    //o que update faria? recebe o valor novo ou o velho?
-    // Ela retorna os valores para o suário ver ou só modifica?
-    //e se eu der ou tiver que fazer um telefone.set?
-    // tem que fazer isso aqui ou muda somente a posição na lista
     public boolean updateTelefone(String rotulo, String numero){
-        if(dadosDeTelefone.contains(new Telefone(rotulo, numero))){
-            addEmail(rotulo,numero);
-            return true;
-        } else{
-            return false;
+        for (Telefone telefone : dadosDeTelefone) {
+            if(telefone.getRotulo().contains(rotulo)){
+                telefone.setNumero(numero);
+                return true;
+            }
         }
+        return false;
     }
 
-    public boolean updateEmail(String rotulo, String numero){
-        if (dadosDeEmail.contains(new Email(rotulo,numero))){
-            addEmail(rotulo, numero);
-            return true;
-        } else {
-            return false;
+    public boolean updateEmail(String rotulo, String email){
+        for (Email emails : dadosDeEmail) {
+            if(emails.getRotulo().contains(rotulo)){
+                emails.setEmail(email);
+                return true;
+            }
         }
+        return false;
     }
 
-    //pq ele remove só orotulo
-    // ao elimiar o rotulo, o email ainda fica
     public boolean removeTelefone(String rotulo){
-
+        for (Telefone telefone : dadosDeTelefone) {
+            if(telefone.getRotulo().contains(rotulo)){
+                dadosDeTelefone.remove(telefone);
+                return true;
+            }
+        }
+        return false;
     }
 
-    public boolean removeEmail(){
-        return true;
+    public boolean removeEmail(String rotulo){
+        for(Email emails : dadosDeEmail){
+            if(emails.getRotulo().contains(rotulo)){
+                dadosDeEmail.remove(emails);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
     }
 }
