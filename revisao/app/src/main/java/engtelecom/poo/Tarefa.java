f59b1d688ab43a11657fb90cbe22858b5e1c3563
Luan de Barros <luanbarrossantos@gmail.com>
2024-07-01 11:26:05 -0300

    retornando as aulas pós greve

---
+++ revisao/app/src/main/java/engtelecom/poo/Tarefa.java
@@ -0,0 +1,69 @@
+package engtelecom.poo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Tarefa {
+    private String nome;
+    private LocalDate dataDeCriacao;
+    private LocalDate dataDeConclusao;
+    private ArrayList<Pessoa>responsaveis;
+    private Grupo grupo;
+
+    public Tarefa(String nome, LocalDate dataDeCriacao) {
+        this.nome = nome;
+        this.dataDeCriacao = dataDeCriacao;
+        this.responsaveis = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "nome='" + nome + '\'' +
+                ", dataDeCriacao=" + dataDeCriacao +
+                ", dataDeConclusao=" + dataDeConclusao +
+                ", responsaveis=" + responsaveis +
+                ", grupo=" + grupo +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDate dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public LocalDate getDataDeConclusao() {
+        return dataDeConclusao;
+    }
+
+    public void setDataDeConclusao(LocalDate dataDeConclusao) {
+        this.dataDeConclusao = dataDeConclusao;
+    }
+
+    public ArrayList<Pessoa> getResponsaveis() {
+        return responsaveis;
+    }
+
+    public void setResponsaveis(ArrayList<Pessoa> responsaveis) {
+        this.responsaveis = responsaveis;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+}
